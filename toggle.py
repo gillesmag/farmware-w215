9ee77d2052fca849244dc218bb589901913aed2a
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2018-05-07 10:06:45 +0200

    connect to smart plug

--- toggle.py
+++ toggle.py
@@ -4,11 +4,19 @@
 Toggles D-link DSP-W215 on and off.
 '''
 
-import pyW215
+from pyW215 import SmartPlug, ON, OFF
 import farmware_tools
 
+import os
+
+farmware_name = "control-w215"
+
+def get_env(key, type_=str):
+    return type_(os.environ["{}_{}".format(farmware_name, key)])
+
 def main():
-    farmware_tools.log("Hello")
+    sp = SmartPlug(get_env("ip_address"), get_env("pin_code"))
+    farmware_tools.log("{} {}".format(get_env("ip_address"), get_env("pin_code"))
 
 if __name__ == "__main__":
     main()
